23d394a3fb3b984838f46c64166b0a501e6594f9
Bryan Tan Chun Xing <productivebryan@gmail.com>
2026-03-16 15:27:36 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAEB4_8xpbkfapQ1k6x3RLIc8B7Ms_Sko4",
+    authDomain: "bryan-s-hub.firebaseapp.com",
+    projectId: "bryan-s-hub",
+    storageBucket: "bryan-s-hub.firebasestorage.app",
+    messagingSenderId: "588737014025",
+    appId: "1:588737014025:web:a578c95196e21e6f53cbd8"
+};
